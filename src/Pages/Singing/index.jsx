c3dfecc48bae89881864b7fddbd2e47f10e955c5
gabriel-vitebo <vitebo@outlook.com>
2023-02-28 15:59:03 -0300

    creating the authentication function

--- src/Pages/Singing/index.jsx
+++ src/Pages/Singing/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { FiLock, FiMail } from "react-icons/fi"
 import { Link } from "react-router-dom"
 import { useAuth } from "../../hooks/auth"
@@ -6,8 +7,14 @@ import { Input } from "../../Components/Input"
 import { Button } from "../../Components/Button"
 
 export function Singing() {
-  const data = useAuth()
-  console.log("meu contexto =>", data)
+  const [email, setEmail] = useState("")
+  const [password, setpassword] = useState("")
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
 
   return (
     <Container>
@@ -16,11 +23,21 @@ export function Singing() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setpassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <div>
           <Link to="/register">Criar conta</Link>
