abb881b24e684c36807adc507f9c3d7def2cb10c
gabriel-vitebo <vitebo@outlook.com>
2023-02-28 14:36:50 -0300

    creating context and Authentication Hook

--- src/Pages/Singing/index.jsx
+++ src/Pages/Singing/index.jsx
@@ -1,10 +1,14 @@
 import { FiLock, FiMail } from "react-icons/fi"
 import { Link } from "react-router-dom"
+import { useAuth } from "../../hooks/auth"
 import { Container, Form, BackgroundImg } from "./styles"
 import { Input } from "../../Components/Input"
 import { Button } from "../../Components/Button"
 
 export function Singing() {
+  const data = useAuth()
+  console.log("meu contexto =>", data)
+
   return (
     <Container>
       <Form>
@@ -12,17 +16,9 @@ export function Singing() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Faça seu login</h2>
 
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
-        />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
 
-        <Input
-          placeholder="Senha"
-          type="password"
-          icon={FiLock}
-        />
+        <Input placeholder="Senha" type="password" icon={FiLock} />
 
         <Button title="Entrar" />
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from "styled-components"
 import GlobalStyle from "./Styles/global"
 import theme from "./Styles/theme"
 import { Routes } from "./routes"
+
+import { AuthProvider } from "./hooks/auth"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
