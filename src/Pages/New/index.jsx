14b221a2935993b768ec7070670c17fa9df7fc3d
gabriel-vitebo <vitebo@outlook.com>
2023-03-06 17:32:52 -0300

    registering notes

--- src/Pages/New/index.jsx
+++ src/Pages/New/index.jsx
@@ -1,4 +1,6 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
+import { api } from "../../services/api"
 
 import { Header } from "../../Components/Header"
 import { TextButton } from "../../Components/TextButton"
@@ -13,8 +15,14 @@ import { useState } from "react"
 import { Container, Form } from "./styles"
 
 export function New() {
+  const [title, setTitle] = useState("")
+  const [rating, setRating] = useState(0)
+  const [description, setDescription] = useState("")
+
   const [tags, setTags] = useState([])
   const [newTag, setNewTag] = useState("")
+
+  const navigate = useNavigate()
 
   function handleAddTag() {
     setTags((prevState) => [...prevState, newTag])
@@ -23,6 +31,18 @@ export function New() {
 
   function handleRemoveTag(deleted) {
     setTags((prevState) => prevState.filter((tag) => tag !== deleted))
+  }
+
+  async function handleNewNote() {
+    await api.post("/movienotes", {
+      title,
+      rating,
+      description,
+      tags,
+    })
+
+    alert("Filme cadastrado com sucesso")
+    navigate("/")
   }
 
   return (
@@ -37,10 +57,20 @@ export function New() {
             <h1>Novo Filme</h1>
           </header>
           <div className="tittle-and-rating">
-            <Input placeholder="Título" />
-            <Input type="number" placeholder="Sua nota (de 0 a 5)" />
+            <Input
+              placeholder="Título"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <Input
+              type="number"
+              placeholder="Sua nota (de 0 a 5)"
+              onChange={(e) => setRating(e.target.value)}
+            />
           </div>
-          <TextArea placeholder="Observações" />
+          <TextArea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
           <h2>Marcadores</h2>
           <div className="tags">
             {tags.map((tag, index) => (
@@ -62,7 +92,7 @@ export function New() {
           </div>
           <div className="SaveAndDeleteButtons">
             <button className="DeleteButton">Excluir filme</button>
-            <Button title="Salvar alterações" />
+            <Button title="Salvar alterações" onClick={handleNewNote} />
           </div>
         </Form>
       </main>
